3baf63824077d64143bd16b588b1b06deecfa41e
aswin7469 <aswinas@pixysos.com>
2022-02-19 17:44:05 +0530

    avicii: import  libnfc-nxp
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Manikantaraavi <raavimanikanta6595@gmail.com>

--- device.mk
+++ device.mk
@@ -50,7 +50,8 @@ PRODUCT_PACKAGES += \
 TARGET_OTA_ASSERT_DEVICE := avicii,OnePlusNord,Nord
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+    $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # tri-state-key
 PRODUCT_PACKAGES += \
